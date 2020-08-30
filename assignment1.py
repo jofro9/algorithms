0554919d75f79bcb40eaa23951bb5ff1e7deaf9e
jfroeli2@uwyo.edu <jfroeli2@uwyo.edu>
2020-08-30 13:54:37 -0600

    Question 2 solution.

--- assignment1.py
+++ assignment1.py
@@ -2,6 +2,8 @@
 # CSCI 3412 - Algorithms
 # Dr. Mazen al Borno
 
+import random
+
 # initialize an empty list and fill
 bulbs = []
 
@@ -11,7 +13,7 @@ for i in range(100):
 switches = len(bulbs)
 
 # make sure initial list is what we are expecting
-print("You have created " + str(switches) + " light bulbs")
+print("\nYou have created " + str(switches) + " light bulbs")
 
 good_list = False
 for i in range(len(bulbs)):
@@ -48,3 +50,45 @@ for person in range(people):
         print("After person " + str(person + 1) + ", there are " + str(on) + " lights on, and " + str(off) + " lights off.")
 
 # Question 2
+
+class RandomInteger:
+    def A(self):
+        return random.randint(0, 5)
+
+    def B(self):
+        return random.randint(0, 8)
+
+    def useBforA(self):
+        A_val = self.B()
+
+        while A_val > 5:
+            A_val = self.B()
+
+        return A_val
+
+    def useAforB(self):
+        B1 = self.A()
+        B2 = self.A()
+
+        while B1 > 4:
+            B1 = self.A()
+
+        while B2 > 4:
+            B2 = self.A()
+
+        return B1 + B2
+
+
+# Part A
+A_rand = RandomInteger()
+print("\nA's random value is: " + str(A_rand.A()))
+
+# Part B
+B_rand = RandomInteger()
+print("B's random value is: " + str(B_rand.B()))
+
+# Part C
+print("A's new value is: " + str(A_rand.useBforA()))
+
+# Part D
+print("B's new value is: " + str(B_rand.useAforB()) + "\n")
